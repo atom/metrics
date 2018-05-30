0e0376c9f0abdb261a88926fa45f9937c9a3115b
Jason Rudolph <jason@jasonrudolph.com>
2018-05-30 15:13:05 -0400

    :shirt: Resolve linter violations

--- spec/metrics-spec.js
+++ spec/metrics-spec.js
@@ -1,10 +1,10 @@
 /** @babel */
 
-import {it, fit, ffit, fffit, beforeEach, afterEach, conditionPromise} from './helpers/async-spec-helpers'
+import {it, fit, ffit, fffit, beforeEach, afterEach, conditionPromise} from './helpers/async-spec-helpers' // eslint-disable-line no-unused-vars
 import Reporter from '../lib/reporter'
 import grim from 'grim'
 
-describe("Metrics", async () => {
+describe('Metrics', async () => {
   let workspaceElement = []
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace)
@@ -12,8 +12,8 @@ describe("Metrics", async () => {
     spyOn(Reporter, 'request')
 
     let storage = {}
-    spyOn(localStorage, 'setItem').andCallFake((key, value) => storage[key] = value)
-    spyOn(localStorage, 'getItem').andCallFake(key => storage[key])
+    spyOn(global.localStorage, 'setItem').andCallFake((key, value) => storage[key] = value)
+    spyOn(global.localStorage, 'getItem').andCallFake(key => storage[key])
 
     Reporter.commandCount = undefined
     spyOn(Reporter, 'consented').andReturn(true)
@@ -21,7 +21,7 @@ describe("Metrics", async () => {
 
   afterEach(async () => await atom.packages.deactivatePackage('metrics'))
 
-  it("reports events", async () => {
+  it('reports events', async () => {
     jasmine.useRealClock()
     await atom.packages.activatePackage('metrics')
     await conditionPromise(() => Reporter.request.callCount === 2)
@@ -35,7 +35,7 @@ describe("Metrics", async () => {
     expect(url).toBeDefined()
   })
 
-  it("reports over SSL", async () => {
+  it('reports over SSL', async () => {
     await atom.packages.activatePackage('metrics')
     await conditionPromise(() => Reporter.request.callCount > 0)
 
@@ -43,7 +43,7 @@ describe("Metrics", async () => {
     expect(url).toMatch('^https:\/\/ssl.google-analytics.com\/collect\?')
   })
 
-  it("reports actual processor architecture", async () => {
+  it('reports actual processor architecture', async () => {
     let expectedArch = process.env.PROCESSOR_ARCHITEW6432 === 'AMD64' ? 'x64' : process.arch
 
     await atom.packages.activatePackage('metrics')
@@ -53,7 +53,7 @@ describe("Metrics", async () => {
     expect(url).toContain(`cd2=${expectedArch}`)
   })
 
-  it("specifies anonymization", async () => {
+  it('specifies anonymization', async () => {
     await atom.packages.activatePackage('metrics')
     await conditionPromise(() => Reporter.request.callCount > 0)
 
@@ -61,71 +61,71 @@ describe("Metrics", async () => {
     expect(url).toContain('&aip=1&')
   })
 
-  it("specifies screen resolution", async () => {
+  it('specifies screen resolution', async () => {
     await atom.packages.activatePackage('metrics')
     await conditionPromise(() => Reporter.request.callCount > 0)
 
     let url = Reporter.request.mostRecentCall.args[0]
-    expect(url).toContain(`&sr=${screen.width}x${screen.height}&`)
+    expect(url).toContain(`&sr=${window.screen.width}x${window.screen.height}&`)
   })
 
-  it("specifies window resolution", async () => {
+  it('specifies window resolution', async () => {
     await atom.packages.activatePackage('metrics')
     await conditionPromise(() => Reporter.request.callCount > 0)
 
     let url = Reporter.request.mostRecentCall.args[0]
-    expect(url).toContain(`&vp=${innerWidth}x${innerHeight}&`)
+    expect(url).toContain(`&vp=${window.innerWidth}x${window.innerHeight}&`)
   })
 
-  it("specifies heap usage in MB and %", async () => {
+  it('specifies heap usage in MB and %', async () => {
     spyOn(process, 'memoryUsage').andReturn({heapTotal: 234567890, heapUsed: 123456789})
 
     await atom.packages.activatePackage('metrics')
     await conditionPromise(() => Reporter.request.callCount > 0)
 
     let url = Reporter.request.mostRecentCall.args[0]
-    expect(url).toContain("&cm1=117&")
-    expect(url).toContain("&cm2=53&")
+    expect(url).toContain('&cm1=117&')
+    expect(url).toContain('&cm2=53&')
   })
 
-  describe("reporting release channel", async () => {
-    beforeEach(() => localStorage.setItem('metrics.userId', 'a'))
+  describe('reporting release channel', async () => {
+    beforeEach(() => global.localStorage.setItem('metrics.userId', 'a'))
 
-    it("reports the dev release channel", async () => {
+    it('reports the dev release channel', async () => {
       spyOn(atom, 'getVersion').andReturn('1.0.2-dev-dedbeef')
 
       await atom.packages.activatePackage('metrics')
       await conditionPromise(() => Reporter.request.callCount > 0)
 
       let url = Reporter.request.mostRecentCall.args[0]
-      expect(url).toContain("aiid=dev")
+      expect(url).toContain('aiid=dev')
     })
 
-    it("reports the beta release channel", async () => {
+    it('reports the beta release channel', async () => {
       spyOn(atom, 'getVersion').andReturn('1.0.2-beta1')
 
       await atom.packages.activatePackage('metrics')
       await conditionPromise(() => Reporter.request.callCount > 0)
 
       let url = Reporter.request.mostRecentCall.args[0]
-      expect(url).toContain("aiid=beta")
+      expect(url).toContain('aiid=beta')
     })
 
-    it("reports the stable release channel", async () => {
+    it('reports the stable release channel', async () => {
       spyOn(atom, 'getVersion').andReturn('1.0.2')
 
       await atom.packages.activatePackage('metrics')
       await conditionPromise(() => Reporter.request.callCount > 0)
 
       let url = Reporter.request.mostRecentCall.args[0]
-      expect(url).toContain("aiid=stable")
+      expect(url).toContain('aiid=stable')
     })
   })
 
-  describe("reporting commands", async () => {
-    describe("when the user is NOT chosen to send commands", async () => {
+  describe('reporting commands', async () => {
+    describe('when the user is NOT chosen to send commands', async () => {
       beforeEach(async () => {
-        localStorage.setItem('metrics.userId', 'a')
+        global.localStorage.setItem('metrics.userId', 'a')
         await atom.packages.activatePackage('metrics')
         await conditionPromise(() => Reporter.request.callCount > 0)
 
@@ -133,7 +133,7 @@ describe("Metrics", async () => {
         mainModule.shouldIncludePanesAndCommands = false
       })
 
-      it("does not watch for commands", async () => {
+      it('does not watch for commands', async () => {
         let command = 'some-package:a-command'
 
         atom.commands.dispatch(workspaceElement, command, null)
@@ -141,35 +141,35 @@ describe("Metrics", async () => {
       })
     })
 
-    describe("when the user is chosen to send commands", async () => {
+    describe('when the user is chosen to send commands', async () => {
       beforeEach(async () => {
-        localStorage.setItem('metrics.userId', 'd')
+        global.localStorage.setItem('metrics.userId', 'd')
         await atom.packages.activatePackage('metrics')
 
         const {mainModule} = atom.packages.getLoadedPackage('metrics')
         mainModule.shouldIncludePanesAndCommands = true
       })
 
-      it("reports commands dispatched via atom.commands", () => {
+      it('reports commands dispatched via atom.commands', () => {
         let command = 'some-package:a-command'
 
         atom.commands.dispatch(workspaceElement, command, null)
         expect(Reporter.commandCount[command]).toBe(1)
 
         let url = Reporter.request.mostRecentCall.args[0]
-        expect(url).toContain("ec=command")
-        expect(url).toContain("ea=some-package")
-        expect(url).toContain("el=some-package%3Aa-command")
-        expect(url).toContain("ev=1")
+        expect(url).toContain('ec=command')
+        expect(url).toContain('ea=some-package')
+        expect(url).toContain('el=some-package%3Aa-command')
+        expect(url).toContain('ev=1')
 
         atom.commands.dispatch(workspaceElement, command, null)
         expect(Reporter.commandCount[command]).toBe(2)
 
         url = Reporter.request.mostRecentCall.args[0]
-        expect(url).toContain("ev=2")
+        expect(url).toContain('ev=2')
       })
 
-      it("does not report editor: and core: commands", () => {
+      it('does not report editor: and core: commands', () => {
         Reporter.request.reset()
         atom.commands.dispatch(workspaceElement, 'core:move-up', null)
         expect(Reporter.request).not.toHaveBeenCalled()
@@ -178,13 +178,13 @@ describe("Metrics", async () => {
         expect(Reporter.request).not.toHaveBeenCalled()
       })
 
-      it("does not report non-namespaced commands", () => {
+      it('does not report non-namespaced commands', () => {
         Reporter.request.reset()
         atom.commands.dispatch(workspaceElement, 'dragover', null)
         expect(Reporter.request).not.toHaveBeenCalled()
       })
 
-      it("does not report vim-mode:* movement commands", () => {
+      it('does not report vim-mode:* movement commands', () => {
         Reporter.request.reset()
         atom.commands.dispatch(workspaceElement, 'vim-mode:move-up', null)
         atom.commands.dispatch(workspaceElement, 'vim-mode:move-down', null)
@@ -193,7 +193,7 @@ describe("Metrics", async () => {
         expect(Reporter.request).not.toHaveBeenCalled()
       })
 
-      it("does not report commands triggered via jquery", () => {
+      it('does not report commands triggered via jquery', () => {
         Reporter.request.reset()
         atom.commands.dispatch(workspaceElement, 'some-package:a-command', {jQueryTrigger: 'trigger'})
         expect(Reporter.request).not.toHaveBeenCalled()
@@ -201,7 +201,7 @@ describe("Metrics", async () => {
     })
   })
 
-  describe("reporting exceptions", async () => {
+  describe('reporting exceptions', async () => {
     beforeEach(async () => {
       spyOn(atom, 'openDevTools').andReturn(Promise.resolve())
       spyOn(atom, 'executeJavaScriptInDevTools')
@@ -209,64 +209,64 @@ describe("Metrics", async () => {
       await conditionPromise(() => Reporter.request.callCount > 0)
     })
 
-    it("reports an exception with the correct type", () => {
+    it('reports an exception with the correct type', () => {
       let message = "Uncaught TypeError: Cannot call method 'getScreenRow' of undefined"
       window.onerror(message, 'abc', 2, 3, {ok: true})
 
       let url = Reporter.request.mostRecentCall.args[0]
-      expect(url).toContain("t=exception")
-      expect(url).toContain("exd=TypeError")
+      expect(url).toContain('t=exception')
+      expect(url).toContain('exd=TypeError')
     })
 
-    describe("when the message has no clear type", () =>
-      it("reports an exception with the correct type", () => {
-        let message = ""
+    describe('when the message has no clear type', () =>
+      it('reports an exception with the correct type', () => {
+        let message = ''
         window.onerror(message, 2, 3, {ok: true})
 
         let url = Reporter.request.mostRecentCall.args[0]
-        expect(url).toContain("t=exception")
-        expect(url).toContain("exd=Unknown")
+        expect(url).toContain('t=exception')
+        expect(url).toContain('exd=Unknown')
       })
     )
 
-    describe("when there are paths in the exception", () => {
-      it("strips unix paths surrounded in quotes", () => {
+    describe('when there are paths in the exception', () => {
+      it('strips unix paths surrounded in quotes', () => {
         let message = "Error: ENOENT, unlink '/Users/someguy/path/file.js'"
         window.onerror(message, 2, 3, {ok: true})
         let url = Reporter.request.mostRecentCall.args[0]
-        expect(decodeURIComponent(url)).toContain("exd=Error: ENOENT, unlink <path>")
+        expect(decodeURIComponent(url)).toContain('exd=Error: ENOENT, unlink <path>')
       })
 
-      it("strips unix paths without quotes", () => {
-        let message = "Uncaught Error: spawn /Users/someguy.omg/path/file-09238_ABC-Final-Final.js ENOENT"
+      it('strips unix paths without quotes', () => {
+        let message = 'Uncaught Error: spawn /Users/someguy.omg/path/file-09238_ABC-Final-Final.js ENOENT'
         window.onerror(message, 2, 3, {ok: true})
         let url = Reporter.request.mostRecentCall.args[0]
-        expect(decodeURIComponent(url)).toContain("exd=Error: spawn <path> ENOENT")
+        expect(decodeURIComponent(url)).toContain('exd=Error: spawn <path> ENOENT')
       })
 
-      it("strips windows paths without quotes", () => {
-        let message = "Uncaught Error: spawn c:\\someguy.omg\\path\\file-09238_ABC-Fin%%$#()al-Final.js ENOENT"
+      it('strips windows paths without quotes', () => {
+        let message = 'Uncaught Error: spawn c:\\someguy.omg\\path\\file-09238_ABC-Fin%%$#()al-Final.js ENOENT'
         window.onerror(message, 2, 3, {ok: true})
         let url = Reporter.request.mostRecentCall.args[0]
-        expect(decodeURIComponent(url)).toContain("exd=Error: spawn <path> ENOENT")
+        expect(decodeURIComponent(url)).toContain('exd=Error: spawn <path> ENOENT')
       })
 
-      it("strips windows paths surrounded in quotes", () => {
+      it('strips windows paths surrounded in quotes', () => {
         let message = "Uncaught Error: EACCES 'c:\\someguy.omg\\path\\file-09238_ABC-Fin%%$#()al-Final.js'"
         window.onerror(message, 2, 3, {ok: true})
         let url = Reporter.request.mostRecentCall.args[0]
-        expect(decodeURIComponent(url)).toContain("exd=Error: EACCES <path>")
+        expect(decodeURIComponent(url)).toContain('exd=Error: EACCES <path>')
       })
     })
   })
 
-  describe("reporting deprecations", async () => {
+  describe('reporting deprecations', async () => {
     beforeEach(async () => {
       await atom.packages.activatePackage('metrics')
       await conditionPromise(() => Reporter.request.callCount > 0)
     })
 
-    it("reports a deprecation with metadata specified", async () => {
+    it('reports a deprecation with metadata specified', async () => {
       Reporter.request.reset()
       jasmine.snapshotDeprecations()
       grim.deprecate('bad things are bad', {packageName: 'somepackage'})
@@ -275,13 +275,13 @@ describe("Metrics", async () => {
       await conditionPromise(() => Reporter.request.callCount > 0)
 
       let url = Reporter.request.mostRecentCall.args[0]
-      expect(url).toContain("t=event")
-      expect(url).toContain("ec=deprecation")
-      expect(url).toContain("ea=somepackage%40unknown")
-      expect(url).toContain("el=bad%20things%20are%20bad")
+      expect(url).toContain('t=event')
+      expect(url).toContain('ec=deprecation')
+      expect(url).toContain('ea=somepackage%40unknown')
+      expect(url).toContain('el=bad%20things%20are%20bad')
     })
 
-    it("reports a deprecation without metadata specified", async () => {
+    it('reports a deprecation without metadata specified', async () => {
       Reporter.request.reset()
       jasmine.snapshotDeprecations()
 
@@ -298,7 +298,7 @@ describe("Metrics", async () => {
         }
       ]
       let deprecation = {
-        message: "bad things are bad",
+        message: 'bad things are bad',
         stacks: [stack]
       }
       grim.addSerializedDeprecation(deprecation)
@@ -311,17 +311,17 @@ describe("Metrics", async () => {
       await conditionPromise(() => Reporter.request.callCount > 0)
 
       let url = Reporter.request.mostRecentCall.args[0]
-      expect(url).toContain("t=event")
-      expect(url).toContain("ec=deprecation")
-      expect(url).toMatch("ea=metrics%40[0-9]+\.[0-9]+\.[0-9]+")
-      expect(url).toContain("el=bad%20things%20are%20bad")
+      expect(url).toContain('t=event')
+      expect(url).toContain('ec=deprecation')
+      expect(url).toMatch('ea=metrics%40[0-9]+\.[0-9]+\.[0-9]+')
+      expect(url).toContain('el=bad%20things%20are%20bad')
     })
   })
 
-  describe("reporting pane items", async () => {
-    describe("when the user is NOT chosen to send events", async () => {
+  describe('reporting pane items', async () => {
+    describe('when the user is NOT chosen to send events', async () => {
       beforeEach(async () => {
-        localStorage.setItem('metrics.userId', 'a')
+        global.localStorage.setItem('metrics.userId', 'a')
         spyOn(Reporter, 'sendPaneItem')
 
         const {mainModule} = await atom.packages.activatePackage('metrics')
@@ -330,16 +330,16 @@ describe("Metrics", async () => {
         await conditionPromise(() => Reporter.request.callCount > 0)
       })
 
-      it("will not report pane items", async () => {
+      it('will not report pane items', async () => {
         await atom.workspace.open('file1.txt')
 
         expect(Reporter.sendPaneItem.callCount).toBe(0)
       })
     })
 
-    describe("when the user IS chosen to send events", async () => {
+    describe('when the user IS chosen to send events', async () => {
       beforeEach(async () => {
-        localStorage.setItem('metrics.userId', 'd')
+        global.localStorage.setItem('metrics.userId', 'd')
         spyOn(Reporter, 'sendPaneItem')
 
         const {mainModule} = await atom.packages.activatePackage('metrics')
@@ -348,7 +348,7 @@ describe("Metrics", async () => {
         await conditionPromise(() => Reporter.request.callCount > 0)
       })
 
-      it("will report pane items", async () => {
+      it('will report pane items', async () => {
         await atom.workspace.open('file1.txt')
 
         expect(Reporter.sendPaneItem.callCount).toBe(1)
@@ -356,9 +356,9 @@ describe("Metrics", async () => {
     })
   })
 
-  describe("when deactivated", async () =>
-    it("stops reporting pane items", async () => {
-      localStorage.setItem('metrics.userId', 'd')
+  describe('when deactivated', async () =>
+    it('stops reporting pane items', async () => {
+      global.localStorage.setItem('metrics.userId', 'd')
       spyOn(Reporter, 'sendPaneItem')
 
       const {mainModule} = await atom.packages.activatePackage('metrics')
@@ -376,7 +376,7 @@ describe("Metrics", async () => {
     })
   )
 
-  describe("the metrics-reporter service", async () => {
+  describe('the metrics-reporter service', async () => {
     let reporterService = null
     beforeEach(async () => {
       await atom.packages.activatePackage('metrics').then(pack => reporterService = pack.mainModule.provideReporter())
@@ -384,22 +384,22 @@ describe("Metrics", async () => {
       Reporter.request.reset()
     })
 
-    describe("::sendEvent", () =>
-      it("makes a request", () => {
+    describe('::sendEvent', () =>
+      it('makes a request', () => {
         reporterService.sendEvent('cat', 'action', 'label')
         expect(Reporter.request).toHaveBeenCalled()
       })
     )
 
-    describe("::sendTiming", () =>
-      it("makes a request", () => {
+    describe('::sendTiming', () =>
+      it('makes a request', () => {
         reporterService.sendEvent('cat', 'name')
         expect(Reporter.request).toHaveBeenCalled()
       })
     )
 
-    describe("::sendException", () =>
-      it("makes a request", () => {
+    describe('::sendException', () =>
+      it('makes a request', () => {
         reporterService.sendException('desc')
         expect(Reporter.request).toHaveBeenCalled()
       })
