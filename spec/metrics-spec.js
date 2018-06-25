d36422dcbf00db5c04f367886188768c1254f85c
Tilde Ann Thurium <annthurium@github.com>
2018-06-25 13:28:50 -0700

    clean up some unnecessary resetting of spies.

--- spec/metrics-spec.js
+++ spec/metrics-spec.js
@@ -31,7 +31,6 @@ describe('Metrics', async () => {
 
   afterEach(async () => {
     atom.packages.deactivatePackage('metrics')
-    Reporter.addCustomEvent.reset()
   })
 
   it('reports consent opt-out changes', async () => {
@@ -406,8 +405,6 @@ describe('Metrics', async () => {
       })
 
       it('will not report pane items', async () => {
-        Reporter.addCustomEvent.reset()
-        Reporter.sendEvent.reset()
 
         await atom.workspace.open('file1.txt')
 
